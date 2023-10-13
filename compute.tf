74418e07a1cc5f570780eab43db0f30f18e30944
Ubuntu <ubuntu@ip-172-31-8-218.eu-west-1.compute.internal>
2023-10-13 12:50:14 +0000

    12

--- compute.tf
+++ compute.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "mtc_main" {
   key_name               = aws_key_pair.mtc_auth.id
   vpc_security_group_ids = [aws_security_group.mtc_sg.id]
   subnet_id              = aws_subnet.mtc_public_subnet[count.index].id
-  user_data              = templatefile("./main-userdata.tpl", { new_hostname = "mtc-main-${random_id.mtc_node_id[count.index].dec}" })
+  # user_data              = templatefile("./main-userdata.tpl", { new_hostname = "mtc-main-${random_id.mtc_node_id[count.index].dec}" })
   root_block_device {
     volume_size = var.main_vol_size
   }
@@ -33,25 +33,36 @@ resource "aws_instance" "mtc_main" {
     Name = "mtc-main-${random_id.mtc_node_id[count.index].dec}"
   }
 
-#   provisioner "local-exec" {
-#     command = "printf '\n${self.public_ip}' >> aws_hosts"
-#   }
-#   provisioner "local-exec" {
-#     when    = destroy
-#     command = "sed -i'/^[0-9]/d' aws_hosts"
-#   }
+  provisioner "local-exec" {
+   command = "printf '\n${self.public_ip}' >> aws_hosts && aws ec2 wait instance-status-ok --instance-ids ${self.id} --region eu-west-1"
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sed -i '/^[0-9]/d' aws_hosts"
+  }
 }
 
-resource "null_resource" "grafana_update" {
-  count = var.main_instance_count
-  provisioner "remote-exec" {
-    inline = ["sudo apt upgrade -y grafana && touch upgrade.log && echo 'I updated Grafa' >> upgrade.log"]
+# resource "null_resource" "grafana_update" {
+#   count = var.main_instance_count
+#   provisioner "remote-exec" {
+#     inline = ["sudo apt upgrade -y grafana && touch upgrade.log && echo 'I updated Grafa' >> upgrade.log"]
 
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = var.TFC_WORKSPACE_SSH_KEY
-      host        = aws_instance.mtc_main[count.index].public_ip
-    }
+#     connection {
+#       type        = "ssh"
+#       user        = "ubuntu"
+#       private_key = var.TFC_WORKSPACE_SSH_KEY
+#       host        = aws_instance.mtc_main[count.index].public_ip
+#     }
+#   }
+# }
+
+resource "null_resource" "grafana_install" {
+  depends_on = [aws_instance.mtc_main]
+  provisioner "local-exec" {
+    command = "ansible-playbook -i aws_hosts --key-file /home/ubuntu/.ssh/mtckey Playbooks/grafana.yml"
   }
+}
+
+output "grafana_access" {
+  value = {for i in aws_instance.mtc_main[*] : i.tags.Name => "${i.public_ip}:3000"}
 }
