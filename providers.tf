13709b495af700f2dc5d7b954bb776e3d12ae679
Ubuntu <ubuntu@ip-172-31-6-136.eu-west-1.compute.internal>
2023-10-15 19:09:02 +0000

    13213

---
+++ providers.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = "eu-west-1"
+  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+}
+
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
