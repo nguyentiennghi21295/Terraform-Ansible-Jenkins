a8e96171881c89ac15f1ff0b744b3824a478f6ba
Ubuntu <ubuntu@ip-172-31-6-136.eu-west-1.compute.internal>
2023-10-18 11:07:23 +0000

    sqdqds

--- compute.tf
+++ compute.tf
@@ -67,3 +67,7 @@ resource "aws_instance" "mtc_main" {
 output "grafana_access" {
   value = {for i in aws_instance.mtc_main[*] : i.tags.Name => "${i.public_ip}:3000"}
 }
+
+output "instance_ips" {
+  value = [for i in aws_instance.mtc_main[*]: i.public_ip]
+}
